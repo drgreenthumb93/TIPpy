18f934e8e71708d248e27ed3bb4c0fa7aad9a00c
[0xz3T] <50826299+drgreenthumb93@users.noreply.github.com>
2022-08-18 08:57:32 +0200

    Create ip_changer.py

---
+++ ip_changer.py
@@ -0,0 +1,17 @@
+import time
+import requests
+from fake_useragent import UserAgent
+from stem import Signal
+from stem.control import Controller
+proxies = {
+    'http': 'socks5://127.0.0.1:9050',
+    'https': 'socks5://127.0.0.1:9050'
+}
+print("Changing IP Address in every 60 seconds....\n\n")
+while True:
+    headers = { 'User-Agent': UserAgent().random }
+    time.sleep(60)
+    with Controller.from_port(port = 9051) as c:
+        c.authenticate()
+        c.signal(Signal.NEWNYM)
+        print(f"Your IP is : {requests.get('https://ident.me', proxies=proxies, headers=headers).text}  ||  User Agent is : {headers['User-Agent']}")
