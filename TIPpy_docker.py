418e44628102e9851e397d5a02efe40dba1fdc1d
drgreentumb <50826299+drgreenthumb93@users.noreply.github.com>
2024-08-29 19:35:31 +0200

    Create TIPpy_docker.py

---
+++ TIPpy_docker.py
@@ -0,0 +1,16 @@
+import time
+from fake_useragent import UserAgent
+from stem import Signal
+from stem.control import Controller
+
+proxies = {
+    'http': 'socks5://127.0.0.1:9050',
+    'https': 'socks5://127.0.0.1:9050'
+}
+
+while True:
+    headers = {'User-Agent': UserAgent().random}
+    time.sleep(30)
+    with Controller.from_port(port=9051) as c:
+        c.authenticate()
+        c.signal(Signal.NEWNYM)
